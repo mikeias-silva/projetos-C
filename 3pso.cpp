f75b3b4f52967b86bb0a43fa6ad23c2936a0b782
mikeias-silva <mikeias26@gmail.com>
2021-08-10 20:56:38 -0300

    pso

--- 3pso.cpp
+++ 3pso.cpp
@@ -22,6 +22,7 @@ int main()
     srand(time(0));
     float x[dimensao] = {0};
     float funcao_x = 0.0;
+    float funcao_pbest = 0.0;
 
     float pbest[dimensao] = {0};
 
@@ -32,15 +33,32 @@ int main()
         for (int j = 0; j < dimensao; j++)
         {
 
-            x[dimensao] = (((float)rand() / (float)(RAND_MAX)*5.0) * -1);
-            printf(" %.2f", x[dimensao]);
+            x[j] = (((float)rand() / (float)(RAND_MAX)*5.0) * -1);
+            printf(" %.2f", x[j]);
+            funcao_x = funcao_x + pow(x[j], 2);
+
+            if (pbest == 0)
+            {
+                pbest[j] = x[j];
+            }
+
+            funcao_pbest = funcao_pbest + pow(pbest[j], 2);
+
+            if (funcao_x < funcao_pbest){
+                pbest[j] = 
+            }
+        }
+        printf("\n Pbest: \n");
+        for (int j = 0; j < dimensao; j++)
+        {
+            printf("%.2f", pbest[j]);
         }
         printf("\n");
-        
-        
+
         //funcao_x = funcao_x + (x[dimensao] * x[dimensao]);
 
-        printf("f(x)%d:", i);
+        printf("f(x)%d: [%.2f]", i, funcao_x);
+        printf("\n");
     }
 
     //funcao_x = funcao_x + (x[dimensao] * x[dimensao]);
